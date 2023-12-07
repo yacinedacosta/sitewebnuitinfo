0ce0c38f5776361b342735149aa188db4e7b1bf2
Enivort <70072436+Enivort@users.noreply.github.com>
2023-12-07 23:55:08 +0100

    custom css

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-import express from "express"
-import path from "path"
-import { engine } from "express-handlebars"
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { engine } from "express-handlebars";
+// require say moudle
+import say from "say";
 
-const app = express()
+const app = express();
 
-app.engine("handlebars", engine())
-app.set("view engine", "handlebars")
-app.set("views", "./src/views")
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.render("home", {
-        title: "Acceuil",
-        home: true
-    })
-})
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
 
-app.get("*", (req, res) => {
-    res.sendStatus(404)
-})
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "Acceuil",
+    home: true,
+  });
+});
+
+// API to change the text to voice over link http://localhost:3000/speak
+app.get("/speak", (req, res) => {
+  let text =
+    "Welcome in our wonderful web site where we will talk about ecology ! yeay yeay yeay yeay !";
+  // below 1.0 is the speed which can be increased
+  say.speak(text, "Alex", 1.0, (err) => {
+    if (err) {
+      return res.json({ message: err, error: true });
+    }
+    return res.json({ message: "Text spoken.", error: false });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendStatus(404);
+});
 
 app.listen(3000, () => {
-        console.log("Server on : ", 3000)
-})
+  console.log("Server on : ", 3000);
+});
