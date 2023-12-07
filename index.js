22002bbadf0672561ff3caa3f882b481cf5cfb12
Balivern <123549102+Balivern@users.noreply.github.com>
2023-12-08 00:00:56 +0100

    index.js mathis

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, 'src/views'));
@@ -24,6 +26,19 @@ app.get('/', (req, res) => {
 app.get("*", (req, res) => {
     res.sendStatus(404)
 })
+
+// API to change the text to voice over link http://localhost:3000/speak
+app.get("/speak", (req, res) => {
+    let text =
+      "Welcome in our wonderful web site where we will talk about ecology ! yeay yeay yeay yeay !";
+    // below 1.0 is the speed which can be increased
+    say.speak(text, "Alex", 1.0, (err) => {
+      if (err) {
+        return res.json({ message: err, error: true });
+      }
+      return res.json({ message: "Text spoken.", error: false });
+    });
+  });
 
 app.listen(3000, () => {
         console.log("Server on : ", 3000)
