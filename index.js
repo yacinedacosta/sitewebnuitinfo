198881112e7574745b022cc7c09fbd58ec02cb71
Balivern <123549102+Balivern@users.noreply.github.com>
2023-12-08 06:57:19 +0100

    fin

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import express from "express";
 import path from "path";
 import { engine } from "express-handlebars";
 import { dirname } from "path";
-import { fileURLToPath } from "url"; // Import fileURLToPath to convert __filename to a path
-import say from "say";
-import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -23,17 +20,6 @@ app.get("/", (req, res) => {
     title: "Acceuil",
     home: true,
   });
-  let text = gettext();
-  console.log(text);
-  // Speak the text
-  // Below 1.0 is the speed, which can be increased
-  say.speak(text, "Alex", 1.0, (err) => {
-    if (err) {
-      console.error("Error speaking:", err);
-    } else {
-      console.log("Text spoken successfully.");
-    }
-  });
 });
 
 app.get("*", (req, res) => {
@@ -43,26 +29,3 @@ app.get("*", (req, res) => {
 app.listen(3000, () => {
   console.log("Server on : ", 3000);
 });
-
-function gettext() {
-  try {
-    // Read the content of main.handlebars synchronously
-    const filePath = path.join(__dirname, "src/views/main.handlebars");
-    const fileContent = fs.readFileSync(filePath, "utf-8");
-
-    // Extract text from the file content (you may need to adjust this based on your actual HTML structure)
-    const text = extractTextFromHTML(fileContent);
-
-    return text;
-  } catch (error) {
-    console.error("Error reading main.handlebars:", error);
-    return "Error reading file.";
-  }
-}
-
-// Function to extract text from HTML content (a simple example, you may need to improve it)
-function extractTextFromHTML(htmlContent) {
-  // Use a library like cheerio or a more sophisticated HTML parsing approach
-  // This is a basic example that removes HTML tags using a regular expression
-  return htmlContent.replace(/<\/?[^>]+(>|$)/g, "");
-}
